4bd2c1408745dc4c1496532498716e5c44ac5a4e
AleDiazV <pellahuenco@gmail.com>
2017-06-14 14:20:51 -0400

    arreglo

--- 2 ciclos/ejercicio10.rb
+++ 2 ciclos/ejercicio10.rb
@@ -1,18 +1,19 @@
 # El siguiente programa debería mostrar un menú
 # e imprimirlo reiteradamente hasta que el usuario ingrese la opción número 4.
 
-ready = 0
 puts 'Ingrese su opcion'
-opcion = gets.chomp.to_i
-resultado = case opcion
-when 1
-  puts "Opción 1: blah"
-when 2
-  puts "Opción 2: blah"
-when 3
-  puts "Opción 3: blah"
-when 4
-  puts "Opción 4: Salir"
-end
 
-puts resultado
+puts "Opción 1: blah"
+puts "Opción 2: blah"
+puts "Opción 3: blah"
+puts "Opción 4: Salir"
+  opcion = gets.chomp.to_i
+
+while(opcion != 4)
+  puts 'Ingrese su opcion'
+  puts "Opción 1: blah"
+  puts "Opción 2: blah"
+  puts "Opción 3: blah"
+  puts "Opción 4: Salir"
+  opcion = gets.chomp.to_i
+end
